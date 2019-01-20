aa0607cd2e7d72db15378df593f7a88025a8b924
kaushal <kaushalk16@gmail.com>
2019-01-20 18:44:16 +0530

    Updated home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,13 @@
-class HomeController < ShopifyApp::AuthenticatedController
+class HomeController < ApplicationController
 
   def index
-  	@state = ShippingWeight.select("state").uniq.order("state")
-  	@sheet_headers = ShippingWeight.select("weight").uniq.order("weight ASC")
-  	@shipping_weights = ShippingWeight.all
-    ss =  ShopifyAPI::CarrierService.find(:all)
-    cs = ss.select{|s| s.name == 'Shipping Custom Rate'}.first
-    if cs.blank?
-      ss =  ShopifyAPI::CarrierService.new()
-      ss.name = "Shipping Custom Rate"
-      ss.callback_url = "https://shipping-custom-rate.herokuapp.com/shipping_cal"
-      ss.service_discovery = true
-      ss.save
-    end
-  	respond_to do |format|
-  		format.html # index.html.erb
-  		format.json
-  	end 
+    @state = ShippingWeight.select("state").uniq.order("state")
+    @sheet_headers = ShippingWeight.select("weight").uniq.order("weight ASC")
+    @shipping_weights = ShippingWeight.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json
+    end  
   end
 
 end
