93830731e2459b1c2e04e54a6a669bf7b727b759
prashantchaudharisuccess <prashant.b.chaudhari@gmail.com>
2019-03-08 17:36:45 +0530

    Seed data for weber shipping rate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if WeberShippingRate.where(:min_qty => 1 && :max_qty => 5).empty?
+	WeberShippingRate.create(:min_qty => 1, :max_qty => 5, :rate => 18.00)
+end
+if WeberShippingRate.where(:min_qty => 6 && :max_qty => 11).empty?
+	WeberShippingRate.create(:min_qty => 6, :max_qty => 11, :rate => 24.00)
+end
+if WeberShippingRate.where(:min_qty => 12 && :max_qty => 23).empty?
+	WeberShippingRate.create(:min_qty => 12, :max_qty => 23, :rate => 32.00)
+end
+if WeberShippingRate.where(:min_qty => 24 && :max_qty => 49).empty?
+	WeberShippingRate.create(:min_qty => 24, :max_qty => 49, :rate => 42.00)
+end
